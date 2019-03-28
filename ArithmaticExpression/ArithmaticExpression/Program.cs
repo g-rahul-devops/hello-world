653bc6a3a961a1ae815c5df9734eb2b93d7d327a
moorthgo <moorthgo@DEVBOX_GOPI>
2019-03-28 04:02:04 -0700

    adding code

---
+++ ArithmaticExpression/ArithmaticExpression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmaticExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
